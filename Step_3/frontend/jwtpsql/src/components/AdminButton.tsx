649aa837713e29b169218ea6b3b2bd95b2dbff4c
Nickolas Piraino <kwnp17c@gmail.com>
2025-11-04 16:18:41 -0500

    practicing Activity component from react 19.2;

--- Step_3/frontend/jwtpsql/src/components/AdminButton.tsx
+++ Step_3/frontend/jwtpsql/src/components/AdminButton.tsx
@@ -1,6 +1,7 @@
 'use client';
 import { useAuth } from "@/context/AuthContext";
 import Link from "next/link"
+// import {Activity} from 'react';
 
 const AdminButton = () => {
     const { user, role } = useAuth();
@@ -8,12 +9,18 @@ const AdminButton = () => {
 
     return (
         <>
-            <Link href={'/admin-dashboard'} className={`${role?.includes('ROLE_ADMIN') ? 'block' : 'hidden'} hover:text-neutral-gray hover:scale-110 duration-300 font-bold py-2 px-4`}>
-                Admin
-            </Link>
-            <Link href={'/dashboard'} className={`${user ? 'block' : 'hidden'} hover:text-neutral-gray hover:scale-110 duration-300 font-bold py-2 px-4`}>
-                Dashboard
-            </Link>
+        {/* Maybe Wrap each in Activity ${role?.includes('ROLE_ADMIN') ? 'block' : 'hidden'}  -    ${user ? 'block' : 'hidden'}*/}
+            {/* <Activity mode={role?.includes('ROLE_ADMIN') ? 'visible' : 'hidden'}> */}
+                <Link href={'/admin-dashboard'} className={`${role?.includes('ROLE_ADMIN') ? 'block' : 'hidden'} hover:text-neutral-gray hover:scale-110 duration-300 font-bold py-2 px-4`}>
+                    Admin
+                </Link>
+            {/* </Activity> */}
+            
+            {/* <Activity mode={user ? 'visible' : 'hidden'}> */}
+                <Link href={'/dashboard'} className={`${user ? 'block' : 'hidden'} hover:text-neutral-gray hover:scale-110 duration-300 font-bold py-2 px-4`}>
+                    Dashboard
+                </Link>
+            {/* </Activity> */}
         </>
     )
 }
