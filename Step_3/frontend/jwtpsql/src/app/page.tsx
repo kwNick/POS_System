d4ce8f2b872373b4dcf914abff28018fa09131d6
Nickolas Piraino <kwnp17c@gmail.com>
2025-11-09 01:19:34 -0500

    creating landing page;

--- Step_3/frontend/jwtpsql/src/app/page.tsx
+++ Step_3/frontend/jwtpsql/src/app/page.tsx
@@ -1,12 +1,38 @@
+import Image from 'next/image';
+
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center w-full min-h-[120vh] font-[family-name:var(--font-geist-sans)]">
 
       {/* landing page */}
-        <section className="min-h-[100vh] w-full flex flex-col items-center justify-center">
-          <h1 className="text-4xl md:text-5xl lg:text-6xl xl:text-6xl font-semibold underline">
-            POS System
+        <section className="min-h-[100vh] w-full flex flex-col gap-y-5 items-center justify-center">
+
+          <h1 className="text-4xl md:text-5xl lg:text-6xl xl:text-7xl font-semibold underline">
+            Sales never been easier
           </h1>
+
+          <h2 className="text-2xl md:text-3xl lg:text-4xl xl:text-5xl">
+            Manage your sales, inventory, and customers all in one place.
+          </h2>
+
+          <div className="flex gap-5">
+            <div className="bg-neutral-surface rounded-lg">
+              <Image src="" width={300} height={600} alt="Picture of POS UI" />
+            </div>
+            <div className=" bg-neutral-surface rounded-lg">
+              <Image src="" width={300} height={600} alt="Picture of POS UI" />
+            </div>
+          </div>
+
+          <div className='flex gap-5'>
+            <button >
+              Get Started
+            </button>
+            <button>
+              Learn More
+            </button>
+          </div>
+          
         </section>
 
         {/* info section */}
