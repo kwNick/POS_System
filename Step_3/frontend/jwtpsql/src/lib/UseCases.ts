06470a02ac496e4f312f60bb622be1fd70334f84
Nickolas Piraino <kwnp17c@gmail.com>
2025-11-10 06:19:07 -0500

    creating use case section;

---
+++ Step_3/frontend/jwtpsql/src/lib/UseCases.ts
@@ -0,0 +1,19 @@
+export const UseCases = [
+    {
+        title: "Dining",
+        description: "Streamline your restaurant operations with our POS system designed for quick order processing and table management."
+    },
+    {
+        title: "Retail",
+        description: "Manage your inventory, sales, and customer data efficiently with our user-friendly POS solution tailored for retail businesses."
+
+    },
+    {
+        title: "Mobile",
+        description: "Take your business on the go with our mobile POS system, perfect for food trucks, pop-up shops, and market stalls."
+    },
+    {
+        title: "Bar and Cafe",
+        description: "Enhance customer experience with our POS system optimized for bars and cafes, featuring quick order entry and payment processing."
+    }
+]
