7c1224a06b69eba1b56f3480c21618a1898148e3
Nickolas Piraino <kwnp17c@gmail.com>
2025-11-11 00:00:04 -0500

    creating sections

--- Step_3/frontend/jwtpsql/src/app/page.tsx
+++ Step_3/frontend/jwtpsql/src/app/page.tsx
@@ -6,41 +6,46 @@ import Link from 'next/link';
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center w-full min-h-[120vh] font-[family-name:var(--font-geist-sans)]">
+    <div className="flex flex-col items-center justify-center w-full h-full font-[family-name:var(--font-geist-sans)]">
 
       {/* landing page */}
-        <section className="relative h-[100vh] w-full flex flex-col gap-y-25 items-center justify-center">
-          <div className='absolute w-1/2 h-1/2 top-[50vh-50%] left-[50vw-50%] rounded-lg bg-primary-purple blur-2xl perspective-origin-bottom perspective-midrange transform-3d -rotate-x-35 z-[-3] mix-blend-color'/>
-          <div className='absolute w-1/2 h-1/2 top-[50vh-50%] left-[50vw-50%] rounded-lg bg-neutral-black blur-2xl perspective-origin-bottom perspective-midrange transform-3d -rotate-x-35 z-[-2] mix-blend-color'/>
-          <div className='absolute w-1/2 h-1/2 top-[50vh-50%] left-[50vw-50%] rounded-lg bg-white blur-3xl perspective-origin-bottom perspective-midrange transform-3d -rotate-x-35 z-[-1] mix-blend-color'/>
+        <section className=" h-[100vh] w-full flex flex-col gap-y-25 items-center justify-center">
 
           <div className='flex flex-col items-center justify-center gap-y-12'>
-            <h1 className=" text-4xl md:text-5xl lg:text-6xl xl:text-7xl font-bold tracking-widest uppercase">
-            POS
-          </h1>
-          <h1 className="absolute inline-block top-0 left-[-10%] text-primary-purple text-4xl md:text-5xl lg:text-6xl xl:text-7xl font-bold tracking-widest uppercase">
-            POS
-          </h1>
-          <h2 className="text-neutral-gray text-2xl md:text-3xl lg:text-4xl xl:text-5xl uppercase">
-            Sales never been easier
-          </h2>
+
+            <div className="relative text-4xl md:text-5xl lg:text-6xl xl:text-7xl font-bold tracking-widest uppercase perspective-near transform-3d -rotate-y-[12.5]">
+              POS
+              <h1 className="absolute top-0 left-[5%] text-cta text-4xl md:text-5xl lg:text-6xl xl:text-7xl font-bold tracking-widest uppercase">
+                POS
+              </h1>
+              <h1 className="absolute top-0 left-[10%] text-primary-purple text-4xl md:text-5xl lg:text-6xl xl:text-7xl font-bold tracking-widest uppercase">
+                POS
+              </h1>
+              <h1 className="absolute top-0 left-[15%] text-primary-blue text-4xl md:text-5xl lg:text-6xl xl:text-7xl font-bold tracking-widest uppercase">
+                POS
+              </h1>
+            </div>
+
+            <h2 className="text-neutral-gray text-2xl md:text-3xl lg:text-4xl xl:text-5xl uppercase">
+              Sales never been easier
+            </h2>
           </div>
 
           <div className='flex gap-5 '>
-            <Link href={'#'} className='p-4 border-1 border-primary-purple rounded-full uppercase hover:text-cta hover:text-shadow-md hover:text-shadow-cta hover:shadow-md hover:shadow-cta hover:scale-118 duration-300'>
+            <Link href={'#'} className='p-4 border-1 border-primary-purple rounded-full uppercase hover:text-cta hover:text-shadow-sm hover:text-shadow-cta hover:shadow-sm hover:shadow-cta hover:scale-118 duration-300'>
               Get Started
             </Link>
 
-            <Link href={'#'} className='p-4 border-1 border-primary-purple bg-neutral-white rounded-full uppercase text-primary-purple hover:text-cta hover:bg-neutral-gray hover:text-shadow-md hover:text-shadow-cta hover:shadow-md hover:shadow-cta hover:scale-118 duration-300'>
+            <Link href={'#'} className='p-4 border-1 border-primary-purple bg-neutral-white rounded-full uppercase text-primary-purple hover:text-cta hover:bg-neutral-gray hover:text-shadow-sm hover:text-shadow-cta hover:shadow-sm hover:shadow-cta hover:scale-118 duration-300'>
               Learn More
             </Link>
           </div>
           
         </section>
 
-        {/* info section */}
+        {/* UseCases section */}
         <section className="h-[110vh] w-full flex flex-col gap-y-8 items-center justify-center bg-neutral-surface z-[-1]">
-          <div className='relative w-full flex flex-col gap-y-4 items-start justify-center border-b-2 border-primary-purple p-5'>
+          <div className='relative w-full h-[15%] flex flex-col gap-y-4 items-start justify-center border-b-2 border-primary-purple p-5'>
             <h1 className="text-primary-purple text-sm md:text-base lg:text-lg xl:text-xl 2xl:text-2xl font-light translate-x-[10vw] uppercase">
               Use Cases
             </h1>
@@ -72,9 +77,9 @@ export default function Home() {
 
         </section>
 
-        {/* features section */}
-        <section className=" h-[240vh] w-full flex flex-col items-center justify-center">
-          <div className='relative w-full h-[10%] flex flex-col gap-y-4 items-start justify-center border-b-2 border-primary-purple'>
+        {/* Features section */}
+        <section className=" h-[300vh] w-full flex flex-col items-center justify-center gap-y-8">
+          <div className='relative w-full h-[15%] flex flex-col gap-y-4 items-start justify-center border-b-2 border-primary-blue p-5'>
             <h1 className="text-primary-purple text-sm md:text-base lg:text-lg xl:text-xl 2xl:text-2xl font-light translate-x-[10vw] uppercase">
               Features
             </h1>
@@ -83,22 +88,39 @@ export default function Home() {
             </h2>
           </div>
 
-          <div className='w-full h-full flex flex-col items-center justify-center gap-y-20 p-10'>
+            {/* Vertical Scroll */}
+          <div className='w-full h-full flex flex-col items-center justify-center gap-y-8 pt-10'>
             {
-              Features.map((feature) => (
-                <div key={feature.title} className='w-3/4 h-1/5 flex flex-col items-start justify-center border-1 border-primary-purple p-5 rounded-lg hover:shadow-md hover:shadow-cta hover:scale-105 duration-300'>
-                  <h3 className='text-xl lg:text-2xl xl:text-3xl font-semibold mb-4'>{feature.title}</h3>
+              Features.slice(0,2).map((feature, idx) => (
+                <div key={feature.title} className='w-[85%] h-[85vh] flex flex-col items-start justify-center bg-neutral-black border-1 border-primary-purple p-5 rounded-lg hover:shadow-md hover:shadow-primary-blue hover:scale-105 duration-300 overflow-hidden'>
+                  <h3 className='text-xl lg:text-2xl xl:text-3xl font-semibold mb-4'><sup className='text-sm'>{idx}</sup>{feature.title}</h3>
                   <p className='text-lg lg:text-xl xl:text-2xl text-neutral-gray'>{feature.description}</p>
                 </div>
               ))
             }
+
+            
           </div>
+
+            {/* Horizontal Scroll */}
+            <div className='w-full h-full overflow-x-hidden flex px-auto'>
+              <div className='w-max h-full flex items-center justify-around gap-x-8 p-15'>
+                  {
+                    Features.slice(2,5).map((feature, idx) => (
+                      <div key={feature.title} className='w-[85vw] h-[85vh] flex flex-col items-start justify-center bg-neutral-black border-1 border-primary-purple p-5 rounded-lg hover:shadow-md hover:shadow-primary-blue hover:scale-105 duration-300 overflow-hidden'>
+                        <h3 className='text-xl lg:text-2xl xl:text-3xl font-semibold mb-4'><sup className='text-sm'>{idx + 2}</sup>{feature.title}</h3>
+                        <p className='text-lg lg:text-xl xl:text-2xl text-neutral-gray'>{feature.description}</p>
+                      </div>
+                    ))
+                  }
+              </div>
+            </div>
 
         </section>
         
-        {/* info section*/}
-        <section className="h-[110vh] w-full flex flex-col items-center justify-center bg-neutral-surface">
-          <div className='relative w-full flex flex-col gap-y-4 items-start justify-center border-b-2 border-primary-purple p-5'>
+        {/* Pricing section*/}
+        <section className=" h-[110vh] w-full flex flex-col items-center justify-center bg-neutral-surface">
+          <div className='relative w-full h-[15%] flex flex-col gap-y-4 items-start justify-center border-b-2 border-primary-blue'>
             <h1 className="text-primary-purple text-sm md:text-base lg:text-lg xl:text-xl 2xl:text-2xl font-light translate-x-[10vw] uppercase">
               Pricing
             </h1>
@@ -107,38 +129,49 @@ export default function Home() {
             </h2>
           </div>
 
-          <div className='flex items-center justify-center w-full h-full p-10'>
-            {PricingPlans.map((plan) => (
-              <div key={plan.name} className='relative flex flex-col items-center justify-start w-1/3 h-4/5 bg-neutral-black border-r-1 border-primary-purple p-5'>
-                <h3 className='text-primary-purple text-xl xl:text-2xl font-semibold mb-10'>{plan.name}</h3>
-                <p className='text-xl lg:text-2xl xl:text-4xl font-bold mb-6'>{plan.price}</p>
-                <ul className='w-full flex flex-col items-start justify-center gap-y-6 text-lg lg:text-xl xl:text-2xl mb-6'>
+          <div className='bg-neutral-black flex items-center justify-center gap-x-6 w-[90%] h-[85%]'>
+            {PricingPlans.map((plan, idx) => (
+              <div key={plan.name} className={`relative flex flex-col items-center justify-start gap-y-10 w-1/3 h-full ${idx == 0 && 'border-l-2'} border-r-2 border-primary-blue p-5`}>
+                <h3 className='text-primary-blue text-2xl xl:text-3xl font-semibold underline'>{plan.name}</h3>
+                <p className='group text-primary-blue text-xl lg:text-2xl xl:text-4xl font-bold underline underline-offset-4 decoration-neutral-gray decoration-2 mb-6'>
+                  <sup className='text-sm text-neutral-gray group-hover:text-cta duration-300'>
+                    {plan.price[0]}
+                  </sup>
+                  {plan.price.substring(1,plan.price.indexOf("/"))}
+                  <span className='text-neutral-gray'>
+                    {plan.price.charAt(plan.price.indexOf("/"))}
+                  </span>
+                  <span className='text-sm text-neutral-gray group-hover:text-cta duration-300'>
+                    {plan.price.substring(plan.price.indexOf("/")+1)}
+                  </span>
+                </p>
+                <ul className='w-full flex flex-col items-start justify-around gap-y-10 text-lg lg:text-xl xl:text-2xl'>
                   {plan.features.map((feature) => (
-                    <li key={feature} className='text-neutral-gray'>• {feature}</li>
+                    <li key={feature} className='group text-neutral-gray'><span className='text-primary-blue group-hover:text-cta'>•</span> {feature}</li>
                   ))}
                 </ul>
-                <Link href={'#'} className='mt-auto p-3 border-1 border-primary-purple rounded-full uppercase hover:text-cta hover:text-shadow-md hover:text-shadow-cta hover:shadow-md hover:shadow-cta hover:scale-118 duration-300'>
+                
+                <div>
+                  <Link href={'#'} className=' p-3 border-1 border-primary-blue rounded-full uppercase hover:text-cta hover:text-shadow-sm hover:text-shadow-cta hover:shadow-sm hover:shadow-cta hover:scale-118 duration-300'>
                   Choose Plan
                 </Link>
+                </div>
               </div>
             ))}
           </div>
 
         </section>
 
-        {/* closing section/call-to-action */}
-        <section className="min-h-[90vh] w-full flex flex-col items-center justify-center">
-          <div className='relative w-full flex flex-col gap-y-4 items-center justify-center p-5'>
+        {/* Closing Section/Call-to-Action */}
+        <section className="h-[90vh] w-full flex flex-col items-center justify-center border-t-1 border-primary-purple">
+          <div className='relative w-full flex flex-col gap-y-4 items-center justify-center'>
 
-            <h1 className="text-primary-purple text-base md:text-lg lg:text-xl xl:text-2xl 2xl:text-3xl font-semibold uppercase tracking-widest">
-              Get <span className='text-neutral-white font-semibold hover:text-cta-hover duration-300'>Started</span> <br/>
-              <span className='text-primary-blue font-semibold hover:text-cta duration-300'>Free</span> Today
+            <h1 className="text-primary-purple text-lg md:text-xl lg:text-lg xl:text-3xl 2xl:text-4xl font-semibold uppercase tracking-widest">
+              Get <span className='text-neutral-white font-semibold hover:text-cta-hover scale-115 duration-300'>Started</span> <br/>
+              <span className='text-primary-blue font-semibold hover:text-cta scale-115 duration-300'>Free</span> Today
             </h1>
-            {/* <h2 className="text-neutral-gray text-base md:text-lg lg:text-xl xl:text-2xl 2xl:text-3xl font-semibold">
-              View Our Options.
-            </h2> */}
 
-            <Link href={'#'} className='mt-5 p-4 border-1 border-primary-purple rounded-full uppercase hover:text-cta hover:text-shadow-md hover:text-shadow-cta hover:shadow-md hover:shadow-cta hover:scale-118 duration-300'>
+            <Link href={'#'} className='mt-5 p-4 border-1 border-primary-purple rounded-full uppercase hover:text-cta hover:text-shadow-sm hover:text-shadow-cta hover:shadow-sm hover:shadow-primary-blue hover:scale-118 duration-300'>
               Sign Up
             </Link>
           </div>
