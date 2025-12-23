47fd70335134d28caa0baa15e27eaf1c2a4d7829
Nickolas Piraino <kwnp17c@gmail.com>
2025-12-22 23:15:29 -0500

    horizontal scale on horiz scroll section;

--- Step_3/frontend/jwtpsql/src/app/page.tsx
+++ Step_3/frontend/jwtpsql/src/app/page.tsx
@@ -108,6 +108,8 @@ export default function Home() {
 
         {/* Closing Section/Call-to-Action */}
         <section className="relative h-[110vh] w-full flex flex-col items-center justify-center border-t-1 border-primary-purple">
+          <div className='absolute inset-0 bg-gradient-to-t from-neutral-white to-transparent bg-[length:_100%_70%] bg-bottom bg-no-repeat opacity-15'/>
+
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320" className='absolute bottom-0 '>
             <path fill="#450693" fillOpacity="1" d="M0,288L48,272C96,256,192,224,288,197.3C384,171,480,149,576,165.3C672,181,768,235,864,250.7C960,267,1056,245,1152,250.7C1248,256,1344,288,1392,304L1440,320L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path>
           </svg>
