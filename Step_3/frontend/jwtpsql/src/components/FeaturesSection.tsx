61725a4187c7d3274ce0727562b6e608acf3ab79
Nickolas Piraino <kwnp17c@gmail.com>
2025-12-25 14:11:14 -0500

    spacing features cards;

--- Step_3/frontend/jwtpsql/src/components/FeaturesSection.tsx
+++ Step_3/frontend/jwtpsql/src/components/FeaturesSection.tsx
@@ -12,17 +12,17 @@ import Image from "next/image";
 const FeaturesSection = () => {
   
     const wrapperRef = useRef<HTMLElement>(null);
-    const horizSections = (Features.slice(2).length-1) * 50;
+    const horizSections = (Features.slice(2).length-1) * 55; //55vw per section
 
     useGSAP(() => {
 
-        gsap.set(wrapperRef.current, {height: `calc(140vh + ${horizSections}vw)`}); //i cut it in half 280 -> 140 + horizSections*100 -> *50
+        gsap.set(wrapperRef.current, {height: `calc(150vh + ${horizSections}vw)`}); //i cut it in half 280 -> 140 + horizSections*100 -> *50
         
     }, []);
 
   return (
       // h-[calc(280vh+200vw)]
-    <section ref={wrapperRef} className="  w-full flex flex-col items-center justify-center gap-y-8 pt-[2.5%] ">
+    <section ref={wrapperRef} className="  w-full flex flex-col items-center justify-center gap-y-8  pt-[2.5%] ">
       <div className='relative w-full h-[15vh] flex flex-col gap-y-4 items-start justify-center border-b-2 border-primary-blue p-5 '>
         <h1 className="text-neutral-white text-sm md:text-base lg:text-lg xl:text-xl 2xl:text-2xl font-light underline underline-offset-4 decoration-primary-purple decoration-2 translate-x-[10vw] uppercase">
           Features
@@ -33,7 +33,7 @@ const FeaturesSection = () => {
       </div>
 
       {/* Vertical Scroll  -- --  bg-[url("/circle-scatter.svg")] bg-cover bg-center*/}
-      <div className='relative w-full h-[200vh] flex flex-col items-center justify-center gap-y-8 pt-10'>
+      <div className='relative w-full h-[200vh] flex flex-col items-center justify-center gap-y-10 lg:gap-y-14 xl:gap-y-16 pt-10'>
         <Image src={'/circle-scatter.svg'} width={1000} height={1000} alt="circle-scatter" className='absolute inset-0 w-full h-full z-[-10]' />
 
         <Image src={'/mesh-gradient-2.png'} width={400} height={500} alt="mesh-gradient-blue-purple" className='absolute -top-1/5 left-[30%] w-full h-full z-[-10] origin-left [filter:_blur(50px)] rotate-90 mix-blend-color'/>
