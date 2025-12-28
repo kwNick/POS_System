68225c5d341d8b76cda5e5c5ad5dc3d415302ebc
Nickolas Piraino <kwnp17c@gmail.com>
2025-12-28 15:16:47 -0500

    home page use case sections animation;

--- Step_3/frontend/jwtpsql/src/components/FeatureCard.tsx
+++ Step_3/frontend/jwtpsql/src/components/FeatureCard.tsx
@@ -2,7 +2,7 @@ const FeatureCard = ({feature, idx}: {feature: {title: string, description: stri
   return (
     <div key={feature.title} className='w-[clamp(30rem,50vw,50rem)] h-[clamp(20rem,40vh,40rem)] flex flex-col items-center justify-center bg-neutral-surface border-1 border-primary-purple p-5 rounded-lg hover:shadow-lg hover:shadow-neutral-white hover:scale-105 duration-300 '>
       
-        <h3 className='text-xl lg:text-2xl xl:text-3xl font-semibold mb-4'><sup className='text-sm'>{idx}</sup>{feature.title}</h3>
+        <h3 className='text-xl lg:text-2xl xl:text-3xl font-semibold mb-4'><sup className='text-sm'>{idx+1}</sup>{feature.title}</h3>
         <p className='text-lg lg:text-xl xl:text-2xl text-neutral-gray'>{feature.description}</p>
     </div>
   )
