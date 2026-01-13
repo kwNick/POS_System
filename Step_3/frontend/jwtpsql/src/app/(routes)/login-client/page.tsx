62d35113f8b0494ed97e808ac6ca54a8da486fe4
Nickolas Piraino <kwnp17c@gmail.com>
2026-01-13 13:41:20 -0500

    register page background and styling;

--- Step_3/frontend/jwtpsql/src/app/(routes)/login-client/page.tsx
+++ Step_3/frontend/jwtpsql/src/app/(routes)/login-client/page.tsx
@@ -5,7 +5,7 @@ export default function LoginPage() {
   return (
     <div className="w-full flex flex-col items-center justify-center gap-y-10 min-h-[120vh]">
 
-      <div className="p-6 lg:p-8 xl:p-12 w-3/5 flex flex-col items-center justify-center bg-neutral-surface ">
+      <div className="p-10 lg:p-12 xl:p-14 w-3/5 flex flex-col items-center justify-center gap-y-10 bg-neutral-surface ">
         <div className="">
           <h2 className="text-4xl md:text-5xl lg:text-6xl xl:text-6xl font-semibold underline">
             Login
--- Step_3/frontend/jwtpsql/src/app/(routes)/register-client/page.tsx
+++ Step_3/frontend/jwtpsql/src/app/(routes)/register-client/page.tsx
@@ -5,13 +5,15 @@ export default function RegisterPage() {
 
     return (
     <div className="w-full flex flex-col items-center justify-center gap-y-10 min-h-[120vh]">
-      <div>
-        <h2 className="text-4xl md:text-5xl lg:text-6xl xl:text-6xl font-semibold underline">
-          Register-Client
-        </h2>
-      </div>
+      <div className="p-10 lg:p-12 xl:p-14 flex flex-col items-center justify-center gap-y-10 w-4/5 bg-neutral-surface">
+          <div>
+          <h2 className="text-4xl md:text-5xl lg:text-6xl xl:text-6xl font-semibold underline">
+            Register
+          </h2>
+        </div>
 
-      <RegisterForm />
+        <RegisterForm />
+      </div>
     </div>
     );
 }
