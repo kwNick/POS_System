0bafe4fda0780733f3a9b5c7a7cc199aa6421e33
Nickolas Piraino <kwnp17c@gmail.com>
2026-02-20 13:06:13 -0500

    styling feature card text;

--- Step_3/frontend/jwtpsql/src/components/FeatureCard.tsx
+++ Step_3/frontend/jwtpsql/src/components/FeatureCard.tsx
@@ -1,9 +1,12 @@
 const FeatureCard = ({feature, idx}: {feature: {title: string, description: string}, idx: number}) => {
   return (
     <div key={feature.title} className='w-[clamp(30rem,50vw,50rem)] h-[clamp(20rem,40vh,40rem)] flex flex-col items-center justify-center bg-neutral-surface border-1 border-primary-purple p-5 rounded-lg hover:shadow-lg hover:shadow-neutral-white hover:scale-105 duration-300 '>
-      
-        <h3 className='text-xl lg:text-2xl xl:text-3xl font-semibold mb-4'><sup className='text-sm'>{idx+1}</sup>{feature.title}</h3>
-        <p className='text-lg lg:text-xl xl:text-2xl text-neutral-gray'>{feature.description}</p>
+      {/* <sup className='text-sm'>{idx+1}</sup> */}
+        <h3 className='text-xl lg:text-2xl xl:text-3xl font-semibold mb-4'>{feature.title}</h3>
+
+        <div className="w-4/5">
+          <p className='text-lg lg:text-xl xl:text-2xl text-neutral-gray '>{feature.description}</p>
+        </div>
     </div>
   )
 }
